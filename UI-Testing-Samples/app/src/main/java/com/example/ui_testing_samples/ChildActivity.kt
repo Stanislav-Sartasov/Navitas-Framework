4a1d0d0417233e4f847a79a455dbb300c1935b88
Vladislav76 <vladislav.myasnikov@bk.ru>
2019-07-18 17:55:13 +0300

    Add navigation Espresso-tests, some renaming

---
+++ UI-Testing-Samples/app/src/main/java/com/example/ui_testing_samples/ChildActivity.kt
@@ -0,0 +1,24 @@
+package com.example.ui_testing_samples
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class ChildActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_child)
+
+        findViewById<Button>(R.id.child_button).setOnClickListener {
+            finish()
+        }
+
+        findViewById<TextView>(R.id.child_title).text = intent.getStringExtra(BUTTON_NAME_EXTRA)
+    }
+
+    companion object {
+        const val BUTTON_NAME_EXTRA = "button_name"
+    }
+}
