ff0d4489bf04d7b52c7705c38c94a4fdda4bc0b9
Vladislav76 <vladislav.myasnikov@bk.ru>
2019-11-03 16:51:08 +0300

    Simple 'Hello World' plugin

---
+++ IDE-Plugin-Sample/src/main/java/HelloWorldAction.java
@@ -0,0 +1,15 @@
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+
+public class HelloWorldAction extends AnAction  {
+
+    public HelloWorldAction() {
+        super("Hello World");
+    }
+
+    public void actionPerformed(AnActionEvent event) {
+        Project project = event.getProject();
+        Messages.showMessageDialog(project, "Hello world!", "Greeting", Messages.getInformationIcon());
+    }
+}
