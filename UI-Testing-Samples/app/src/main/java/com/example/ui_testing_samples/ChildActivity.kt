d2d091cae529e46d13175dfaef70008ccc8ca727
Vladislav76 <vladislav.myasnikov@bk.ru>
2020-04-24 23:40:33 +0300

    Add a few nested methods

--- UI-Testing-Samples/app/src/main/java/com/example/ui_testing_samples/ChildActivity.kt
+++ UI-Testing-Samples/app/src/main/java/com/example/ui_testing_samples/ChildActivity.kt
@@ -18,6 +18,34 @@ class ChildActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.child_title).text = intent.getStringExtra(BUTTON_NAME_EXTRA)
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        executeSomething()
+        executeSomething()
+    }
+       
+    override fun onStop() {
+        super.onStop()
+        
+        rec(10)
+    }
+
+    private fun executeSomething(): Long {
+        var x = 0L
+        for (i in 0..1000) {
+            for (j in 0..1000) {
+                x += (i + j)
+            }
+        }
+        return x
+    }
+    
+    private fun rec(value: Int) {
+        if (value > 0) 
+            rec(value - 1)
+    }
+
     companion object {
         const val BUTTON_NAME_EXTRA = "button_name"
     }
