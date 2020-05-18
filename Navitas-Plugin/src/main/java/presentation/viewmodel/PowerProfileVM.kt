ff3686b0f5481bf861edc24aaa6f2f6d9191905e
Egor Gordienko <egor.gordienko.00@gmail.com>
2020-05-18 18:48:39 +0300

    add gui form, add profile parsing to ViewModel instead of View

--- Navitas-Plugin/src/main/java/presentation/viewmodel/PowerProfileVM.kt
+++ Navitas-Plugin/src/main/java/presentation/viewmodel/PowerProfileVM.kt
@@ -3,12 +3,21 @@ package presentation.viewmodel
 import data.model.PowerProfile
 import domain.repository.PowerProfileRepository
 import io.reactivex.Observable
+import tooling.PowerProfileManager
+import tooling.PowerProfileParser
+import java.io.File
 
 class PowerProfileVM(private val powerProfileRepository: PowerProfileRepository) {
 
     val powerProfile: Observable<PowerProfile> = powerProfileRepository.fetch()
 
-    fun save(profile: PowerProfile) {
+    fun saveDefault() {
+        val profile = PowerProfileManager.getDefaultProfile()
+        powerProfileRepository.save(profile)
+    }
+
+    fun save(profileFile: File) {
+        val profile = PowerProfileParser(profileFile).parseXML()
         powerProfileRepository.save(profile)
     }
 }
