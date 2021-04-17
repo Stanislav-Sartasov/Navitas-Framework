a7b57dd7b91d8af2e4230e70bb5af75f5cba60d9
kznts9v-1lya <kuznetsov.ilya.alexandrovich@gmail.com>
2021-04-17 18:47:11 +0300

    Custom power porfile choice almost fixed

--- Navitas-Plugin/src/main/kotlin/tooling/PowerProfileParser.kt
+++ Navitas-Plugin/src/main/kotlin/tooling/PowerProfileParser.kt
@@ -22,17 +22,30 @@ class PowerProfileParser(private val file: File) {
         val doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file)
         val xPath = XPathFactory.newInstance().newXPath()
 
-        val clusterExpr = xPath.compile("//array[contains(@name, 'clusters.cores')]")
-        val clusterNode = clusterExpr.evaluate(doc, XPathConstants.NODE) as Node
-        val clusterNodeList = xPath.evaluate("value", clusterNode, XPathConstants.NODESET) as NodeList
+        val clusterExpr = xPath.compile("//array[contains(@name, 'cpu.clusters.cores')]")
+        val clusterNode = clusterExpr.evaluate(doc, XPathConstants.NODE)
+        if(clusterNode != null) {
+            val clusterNodeList = xPath.evaluate("value", clusterNode as Node, XPathConstants.NODESET) as NodeList
 
-        for (i in 0 until clusterNodeList.length) {
-            val cluster = CpuCoreCluster(clusterNodeList.item(i).textContent.toInt())
+            for (i in 0 until clusterNodeList.length) {
+                val cluster = CpuCoreCluster(clusterNodeList.item(i).textContent.toInt())
+                clusters.add(cluster)
+            }
+        }
+        else {
+            // TODO: get numCores in another way
+                //  If this number is not correct for the device, there will be exceptions
+                //  Let it be 4 cores like on Samsung A3 2016 by default
+            val cluster = CpuCoreCluster(4)
             clusters.add(cluster)
         }
 
         for (i in 0 until clusters.size) {
-            val speedsExpr = xPath.compile("//array[contains(@name, 'speeds.cluster$i')]")
+            val speedsExpr = if(clusters.size == 1) {
+                xPath.compile("//array[contains(@name, 'cpu.speeds')]")
+            } else {
+                xPath.compile("//array[contains(@name, 'cpu.speeds.cluster$i')]")
+            }
             val speedsNode = speedsExpr.evaluate(doc, XPathConstants.NODE) as Node
             val speedsNodeList = xPath.evaluate("value", speedsNode, XPathConstants.NODESET) as NodeList
 
@@ -45,7 +58,11 @@ class PowerProfileParser(private val file: File) {
                 clusters[i].speeds.add(speed)
             }
 
-            val powersExpr = xPath.compile("//array[contains(@name, 'active.cluster$i')]")
+            val powersExpr = if(clusters.size == 1) {
+                xPath.compile("//array[contains(@name, 'cpu.active')]")
+            } else {
+                xPath.compile("//array[contains(@name, 'cpu.active.cluster$i')]")
+            }
             val powersNode = powersExpr.evaluate(doc, XPathConstants.NODE) as Node
             val powersNodeList = xPath.evaluate("value", powersNode, XPathConstants.NODESET) as NodeList
 
